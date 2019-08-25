925c77957a1b74fb69ecc9175018712015db6c13
Hawns <adam81501@gmail.com>
2019-08-25 14:27:39 -0600

    Added dialog system

---
+++ Assets/Scripts/Dialog.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class Dialog : MonoBehaviour
+{
+    public TextMeshProUGUI textDisplay;
+    public string[] lines;
+    private int sentenceindex;
+    public float TimeBetweenLetters;
+    private bool inDialog;
+
+
+    private void Start()
+    {
+        StartCoroutine(TypeSentences());
+    }
+
+    public IEnumerator TypeSentences()
+    {
+        textDisplay.text = "";
+        inDialog = true;
+        foreach (char letters in lines[sentenceindex].ToCharArray())
+        {
+            textDisplay.text += letters;
+            yield return new WaitForSeconds(TimeBetweenLetters);
+        }
+
+        inDialog = false;
+    }
+
+    public void ContinueDialog()
+    {
+        if (!inDialog)
+        {
+            if (sentenceindex < lines.Length - 1)
+            {
+                sentenceindex++;
+                StartCoroutine(TypeSentences());
+            } else if (sentenceindex == lines.Length - 1)
+            {
+                EndDialog();
+            }
+        }
+    }
+
+    void EndDialog()
+    {
+        Debug.Log("Ending Dialog");
+    }
+}
