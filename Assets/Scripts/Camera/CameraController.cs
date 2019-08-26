2c34757b3e4e8df3ed744128b9c60744b0e800d6
Sean McElholm <seanmcelholm@gmail.com>
2019-08-26 17:37:51 +0100

    Add a basic main menu, and relevent sprites. Not fully working at the moment.

---
+++ Assets/Scripts/Camera/CameraController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public Transform m_Player;
+    private Camera m_Camera;
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        m_Player = PlayerManager.Instance.movement.transform;
+        m_Camera = Camera.main;
+    }
+
+    // Update is called once per frame
+    void LateUpdate()
+    {
+        m_Camera.transform.position = new Vector3(m_Player.position.x, m_Camera.transform.position.y, m_Camera.transform.position.z);
+    }
+}
