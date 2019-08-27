484873699cd57b0259f63e96b303a996597d7df7
Hawns <adam81501@gmail.com>
2019-08-27 12:58:14 -0600

    Added functionality to the gamemanager

--- Assets/Scripts/Managers/GameManager.cs
+++ Assets/Scripts/Managers/GameManager.cs
@@ -26,4 +26,24 @@ public class GameManager : MonoBehaviour
 
     [HideInInspector] public GameObject player;
 
+    public void StartGame()
+    {
+        StartCoroutine(StartingGame());
+    }
+
+    IEnumerator StartingGame()
+    {
+        Fader.instance.FadeOut();
+        Time.timeScale = 0;
+        yield return new WaitForSecondsRealtime(1f);
+        Fader.instance.FadeIn();
+        yield return new WaitForSecondsRealtime(1f);
+        Time.timeScale = 1f;
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
 }
