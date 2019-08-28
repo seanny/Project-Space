17ba6d59b0aae105cad7a5d73f6cb29759310fe5
Hawns <adam81501@gmail.com>
2019-08-28 16:45:42 -0600

    Fixed chinemachine

---
+++ Assets/CameraFollow.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class CameraFollow : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        GetComponent<CinemachineVirtualCamera>().Follow = GameManager.instance.player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
